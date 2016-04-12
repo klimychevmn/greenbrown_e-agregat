dc19e8c89850784861b37edfd378766a59ab1090
klimychevmn <plagonist@gmail.com>
2016-04-12 16:54:54 +0300

    navbar stanki

---
+++ stanki/templates/main.php
@@ -0,0 +1,9 @@
+<?php require('header.php');?>
+<?php require('navigate.php');?>
+
+<div class="wrapper">
+    <div>dklgjlsdjkgl</div>
+</div>
+
+
+<?php require('footer.php');?>
---
+++ stanki/templates/navigate.php
@@ -0,0 +1,121 @@
+<!-- NAVBAR
+================================================== -->
+<body>
+<div id="wrapper">
+    <div style="background-color: #e5e5e5;height: 30px;margin-bottom: 10px;" class="phones">
+        <div class="wrapper" style="background-color: #e5e5e5; padding-top: 5px">
+            <div class="row" style="font-size: 13px; text-align: center;">
+                <div class="col-lg-3" style="border-right: 1px solid #ffffff"><span
+                        style="color: red">+7 495 133-96-62</span> Москва
+                </div>
+                <div class="col-lg-3" style="border-right: 1px solid #ffffff"><span
+                        style="color: red">+7 343 386-19-37</span> Екатеринбург
+                </div>
+                <div class="col-lg-3" style="border-right: 1px solid #ffffff"><span
+                        style="color: red"> +7 843 202-41-37</span> Казань
+                </div>
+                <div class="col-lg-3"><span style="color: red">+7 863 209-89-57</span> Ростов на Дону</div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper logo_baner" style="margin-bottom: 10px">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12" style="margin-top: 10px">
+                <img src="../img/logo1.jpg" alt="">
+            </div>
+            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                <img src="../img/logo2.jpg" alt="">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script>
+                <div style="display: inline-block; text-align: right; width: 100%;margin:10px" class="yashare-auto-init"
+                     data-yasharel10n="ru" data-yasharetype="none"
+                     data-yasharequickservices="vkontakte,facebook,twitter,odnoklassniki,lj,gplus"><span
+                        class="b-share"><a
+                            rel="nofollow" target="_blank" title="ВКонтакте"
+                            class="b-share__handle b-share__link b-share-btn__vkontakte"
+                            href="https://share.yandex.net/go.xml?service=vkontakte&amp;url=http%3A%2F%2Fwww.stanki-zavod.ru%2F&amp;title=%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%20%D0%BA%D1%83%D0%B7%D0%BD%D0%B5%D1%87%D0%BD%D0%BE-%D0%BF%D1%80%D0%B5%D1%81%D1%81%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%20%D0%B8%20%D0%BC%D0%B5%D1%82%D0%B0%D0%BB%D0%BB%D0%BE%D1%80%D0%B5%D0%B6%D1%83%D1%89%D0%B5%D0%B3%D0%BE%20%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F."
+                            data-service="vkontakte"><span class="b-share-icon b-share-icon_vkontakte"></span></a><a
+                            rel="nofollow" target="_blank" title="Facebook"
+                            class="b-share__handle b-share__link b-share-btn__facebook"
+                            href="https://share.yandex.net/go.xml?service=facebook&amp;url=http%3A%2F%2Fwww.stanki-zavod.ru%2F&amp;title=%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%20%D0%BA%D1%83%D0%B7%D0%BD%D0%B5%D1%87%D0%BD%D0%BE-%D0%BF%D1%80%D0%B5%D1%81%D1%81%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%20%D0%B8%20%D0%BC%D0%B5%D1%82%D0%B0%D0%BB%D0%BB%D0%BE%D1%80%D0%B5%D0%B6%D1%83%D1%89%D0%B5%D0%B3%D0%BE%20%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F."
+                            data-service="facebook"><span class="b-share-icon b-share-icon_facebook"></span></a><a
+                            rel="nofollow" target="_blank" title="Twitter"
+                            class="b-share__handle b-share__link b-share-btn__twitter"
+                            href="https://share.yandex.net/go.xml?service=twitter&amp;url=http%3A%2F%2Fwww.stanki-zavod.ru%2F&amp;title=%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%20%D0%BA%D1%83%D0%B7%D0%BD%D0%B5%D1%87%D0%BD%D0%BE-%D0%BF%D1%80%D0%B5%D1%81%D1%81%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%20%D0%B8%20%D0%BC%D0%B5%D1%82%D0%B0%D0%BB%D0%BB%D0%BE%D1%80%D0%B5%D0%B6%D1%83%D1%89%D0%B5%D0%B3%D0%BE%20%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F."
+                            data-service="twitter"><span class="b-share-icon b-share-icon_twitter"></span></a><a
+                            rel="nofollow"
+                            target="_blank"
+                            title="Одноклассники"
+                            class="b-share__handle b-share__link b-share-btn__odnoklassniki"
+                            href="https://share.yandex.net/go.xml?service=odnoklassniki&amp;url=http%3A%2F%2Fwww.stanki-zavod.ru%2F&amp;title=%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%20%D0%BA%D1%83%D0%B7%D0%BD%D0%B5%D1%87%D0%BD%D0%BE-%D0%BF%D1%80%D0%B5%D1%81%D1%81%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%20%D0%B8%20%D0%BC%D0%B5%D1%82%D0%B0%D0%BB%D0%BB%D0%BE%D1%80%D0%B5%D0%B6%D1%83%D1%89%D0%B5%D0%B3%D0%BE%20%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F."
+                            data-service="odnoklassniki"><span
+                                class="b-share-icon b-share-icon_odnoklassniki"></span></a><a rel="nofollow"
+                                                                                              target="_blank"
+                                                                                              title="LiveJournal"
+                                                                                              class="b-share__handle b-share__link b-share-btn__lj"
+                                                                                              href="https://share.yandex.net/go.xml?service=lj&amp;url=http%3A%2F%2Fwww.stanki-zavod.ru%2F&amp;title=%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%20%D0%BA%D1%83%D0%B7%D0%BD%D0%B5%D1%87%D0%BD%D0%BE-%D0%BF%D1%80%D0%B5%D1%81%D1%81%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%20%D0%B8%20%D0%BC%D0%B5%D1%82%D0%B0%D0%BB%D0%BB%D0%BE%D1%80%D0%B5%D0%B6%D1%83%D1%89%D0%B5%D0%B3%D0%BE%20%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F."
+                                                                                              data-service="lj"><span
+                                class="b-share-icon b-share-icon_lj"></span></a><a rel="nofollow" target="_blank"
+                                                                                   title="Google Plus"
+                                                                                   class="b-share__handle b-share__link b-share-btn__gplus"
+                                                                                   href="https://share.yandex.net/go.xml?service=gplus&amp;url=http%3A%2F%2Fwww.stanki-zavod.ru%2F&amp;title=%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%20%D0%BA%D1%83%D0%B7%D0%BD%D0%B5%D1%87%D0%BD%D0%BE-%D0%BF%D1%80%D0%B5%D1%81%D1%81%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%20%D0%B8%20%D0%BC%D0%B5%D1%82%D0%B0%D0%BB%D0%BB%D0%BE%D1%80%D0%B5%D0%B6%D1%83%D1%89%D0%B5%D0%B3%D0%BE%20%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F."
+                                                                                   data-service="gplus"><span
+                                class="b-share-icon b-share-icon_gplus"></span></a></span></div>
+                <div style="display: inline-block; text-align: right; width: 100%;margin:10px">Звонок по России
+                    бесплатно
+                </div>
+                <div style="display: inline-block; text-align: right; width: 100%;margin:10px">8-800-700-29-55</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="navbar-wrapper" style="clear: both">
+        <div class="wrapper">
+            <div class="navbar navbar-inverse">
+                <div class="container">
+                    
+                    <div class="navbar-header navmobile">
+                        <div style="float: left;">
+                           <div style="font-size: 10px">Звонок по России бесплатно</div>
+                           <div style="font-size: 20px">8-800-700-29-55</div>
+                        </div>
+                        <div style="float: right">
+
+                            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                    data-target=".navbar-collapse" style="float: right; margin: 0px 0px -10px;">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+
+                            <a href="#" style="float: right; margin: 3px"><span class="glyphicon glyphicon-phone"
+                                                                   style="font-size: 20px; color: #ffffff"></span></a>
+                            <a href="#" style="float: right; margin: 3px"><span class="glyphicon glyphicon-search"
+                                                                   style="font-size: 20px; color: #ffffff"></span></a>
+                        </div>
+                    </div>
+
+                    <div class="navbar-collapse collapse" style="margin-left: 3%">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">Главная</a></li>
+                            <li><a href="#">Продукция</a></li>
+                            <li><a href="#">Запчасти</a></li>
+                            <li><a href="#">Упаковка</a></li>
+                            <li><a href="#">Доставка</a></li>
+                            <li><a href="#">Сервис</a></li>
+                            <li><a href="#">Котакты</a></li>
+                            <li><a href="#">Партнеры</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="col-xs-12 mobile_logo"  style="margin:20px 0px">
+        <span class="col-xs-4"><img style="width: 110%" src="../img/logo1.jpg" alt=""></span>
+        <span class="col-xs-8"><img style="width: 110%" src="../img/logo2.jpg" alt=""></span>
+    </div>
