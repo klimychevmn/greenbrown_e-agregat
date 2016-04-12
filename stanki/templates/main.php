036d9999f560a9d10b7677f954eb61c96331975a
klimychevmn <plagonist@gmail.com>
2016-04-12 19:18:42 +0300

    main page catalog

--- stanki/templates/main.php
+++ stanki/templates/main.php
@@ -16,7 +16,56 @@
 </div>
 
 <div class="wrapper">
-    <div class="content">dklgjlsdjkgl</div>
+    <div class="content" style="margin-top: -20px;">
+        <div>
+            <div class="row" style="margin-left: 10px">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="margin-bottom: 20px; margin-left: -10px;">
+                    <a href="#">
+                        <img src="../img/catalog_img/catalog1.jpg" alt="" style="width: 100%;margin-bottom: 10px">
+                        <div style="text-align: center;text-decoration: underline; color: #d9251d">ОБОРУДОВАНИЕ ДЛЯ ГИБКИ ЛИСТОВОГО МЕТАЛЛА</div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="margin-bottom: 20px; margin-left: -10px;">
+                    <a href="#">
+                        <img src="../img/catalog_img/catalog2.jpg" alt="" style="width: 100%; margin-bottom: 30px">
+                        <div style="text-align: center;text-decoration: underline; color: #d9251d">ОБОРУДОВАНИЕ ДЛЯ РАСКРОЯ ЛИСТОВОГО МЕТАЛЛА</div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="margin-bottom: 20px; margin-left: -10px;">
+                    <a href="#">
+                        <img src="../img/catalog_img/catalog3.jpg" alt="" style="width: 100%; margin-bottom: 30px">
+                        <div style="text-align: center;text-decoration: underline; color: #d9251d">ОБОРУДОВАНИЕ ДЛЯ ВЫПОЛНЕНИЯ КУЗНЕЧНЫХ РАБОТ</div>
+                    </a>
+                </div>
+            </div>
+            <div class="row" style="margin-left: 10px">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="margin-bottom: 20px; margin-left: -10px;">
+                    <a href="#">
+                        <img src="../img/catalog_img/catalog4.jpg" alt="" style="width: 100%; margin-bottom: 30px">
+                        <div style="text-align: center;text-decoration: underline; color: #d9251d">ОБОРУДОВАНИЕ ДЛЯ ХОЛОДНОЙ ШТАМПОВКИ ЛИСТОВОГО МЕТАЛЛА</div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="margin-bottom: 20px; margin-left: -10px;">
+                    <a href="#">
+                        <img src="../img/catalog_img/catalog5.jpg" alt="" style="width: 100%; margin-bottom: 30px">
+                        <div style="text-align: center;text-decoration: underline; color: #d9251d">ОБОРУДОВАНИЕ ДЛЯ ЛИСТОВОГО И ПРОФИЛЬНОГО РАСКРОЯ МЕТАЛЛА</div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="margin-bottom: 20px; margin-left: -10px;">
+                    <a href="#">
+                        <img src="../img/catalog_img/catalog6.jpg" alt="" style="width: 100%; margin-bottom: 30px">
+                        <div style="text-align: center;text-decoration: underline; color: #d9251d">ИЗГОТОВЛЕНИЕ ПРОФНАСТИЛА МЕТАЛЛОЧЕРЕПИЦЫ И СЕТКИ ЦВПС</div>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="catalogBtn">ПОСМОТРЕТЬ ВСЮ ПРОДУКЦИЮ</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </div>
 
 
