6fb4e6aa4d2b9c31bb8e9f845ff4bcd9938d2885
klimychevmn <plagonist@gmail.com>
2016-04-14 14:36:17 +0300

    item right colum

--- stanki/templates/item.php
+++ stanki/templates/item.php
@@ -1,13 +1,84 @@
-<?php require('header.php');?>
-<?php require('navigate.php');?>
+<?php require('header.php'); ?>
+<?php require('navigate.php'); ?>
+
 
     <div class="content">
+        <div>
 
+            <div class="row" style="padding-top: 30px">
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                    <div style="height: 500px">
+                        kfjhkasjfhkasjhfkjashfkjhskfjhaskjfhkjahsf
+                        kfjhkasjfhkasjhfkjashfkjhskfjhaskjfhkjahsf
+                        kfjhkasjfhkasjhfkjashfkjhskfjhaskjfhkjahsf
+                        kfjhkasjfhkasjhfkjashfkjhskfjhaskjfhkjahsf
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
 
+                    <div style="margin: 20px auto">
+                        <h5 class="offer-title">ПОХОЖИЕ ПРЕДЛОЖЕНИЯ</h5>
+                        <div class="offer-item">
+                            <a>
+                                <span>СМЖ652 </span> Для резки круга, уголка, полосы, швуллера,
+                                пробивки отверстий
+                            </a>
+                        </div>
+                        <div class="offer-item">
+                            <a>
+                                <span>СМЖ652 </span> Для резки круга, уголка, полосы, швуллера,
+                                пробивки отверстий
+                            </a>
+                        </div>
+                        <div class="offer-item">
+                            <a>
+                                <span>СМЖ652 </span> Для резки круга, уголка, полосы, швуллера,
+                                пробивки отверстий
+                            </a>
+                        </div>
+                        <div class="offer-item">
+                            <a>
+                                <span>СМЖ652 </span> Для резки круга, уголка, полосы, швуллера,
+                                пробивки отверстий
+                            </a>
+                        </div>
+                    </div>
 
-        <div class="row tab_panel">
-            <?php require('tab-panel.php');?>
+                    <form class="catalog_form">
+                        <h4>ФОРМА ЗАКАЗА</h4>
+                        <input type="text" placeholder="Контактное лицо">
+                        <input type="text" placeholder="Организация">
+                        <input type="text" placeholder="Телефон">
+                        <input type="text" placeholder="Электронная почта">
+                        <textarea name="" id="" cols="22" rows="6" placeholder="Запрос"></textarea>
+                        <button type="submit">ОТПРАВИТЬ ЗАКАЗ</button>
+                    </form>
+
+                    <div style="margin: 20px auto">
+                        <div class="contact">
+                            <div class="contact-name">Рыбников Евгений Евгеньевич
+                            </div>
+                            <div class="contact-job">коммерческий директор</div>
+                            <div class="contact-phone">8-912-344-26-06</div>
+                            <a style="text-decoration: underline">ree-kpo@mail.ru</a>
+                        </div>
+                        <div class="contact">
+                            <div class="contact-name">Оленко Юлия Сергеевна</div>
+                            <div class="contact-job">специалист отдела сбыта</div>
+                            <div class="contact-phone">8-905-841-08-81</div>
+                            <a style="text-decoration: underline">taa-kpo@mail.ru</a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="tab_panel row">
+                <?php require('tab-panel.php'); ?>
+            </div>
+
         </div>
     </div>
 
-<?php require('footer.php');?>
+<?php require('footer.php'); ?>
