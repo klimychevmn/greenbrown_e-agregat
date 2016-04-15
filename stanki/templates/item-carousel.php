a86f4ebf3f01eace60b07478b73d5f32d715c26a
klimychevmn <plagonist@gmail.com>
2016-04-15 11:36:22 +0300

    carousel item

---
+++ stanki/templates/item-carousel.php
@@ -0,0 +1,23 @@
+<div id="myCarousel" class="carousel slide">
+
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="item active">
+                <img src="../img/item-carousel/item1.jpg" alt="First slide">
+        </div>
+        <div class="item">
+                <img src="../img/item-carousel/item1.jpg" alt="Second slide">
+        </div>
+    </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <img src="../img/item-carousel/left.jpg" alt="Левая стрелка">
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <img src="../img/item-carousel/right.jpg" alt="Правая стрелка" >
+    </a>
+</div>
+
+
