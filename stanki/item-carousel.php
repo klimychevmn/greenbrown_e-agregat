df4eba81720f597ac5248adcbd03cc880b2ff71e
klimychevmn <plagonist@gmail.com>
2016-04-21 15:23:14 +0300

    fixed carousel + tab-panel

--- stanki/item-carousel.php
+++ stanki/item-carousel.php
@@ -4,18 +4,33 @@
         <div class="carousel-inner" >
             <div class="item active" >
                 <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/img//kpp/molot/6.jpg">
+                <div class="carousel-caption">
+                    <span>Hello</span>
+                </div>
             </div>
             <div class="item" >
                 <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_2tzqc6vcobaiewt" >
+                <div class="carousel-caption">
+                    <span>My friend</span>
+                </div>
             </div>
             <div class="item" >
                 <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_7gdscjd36y73tau" >
+                <div class="carousel-caption">
+                    <span>Glad to see</span>
+                </div>
             </div>
             <div class="item" >
                 <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_xvl5iixg3qt7aw4" >
+                <div class="carousel-caption">
+                    <span>You!</span>
+                </div>
             </div>
             <div class="item" >
                 <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_leunhqd2e4h3gp0" >
+                <div class="carousel-caption">
+                    <span>:)</span>
+                </div>
             </div>
         </div>
         <!--END-->
@@ -30,12 +45,12 @@
         <!--END-->
         <!--LEFT-->
         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <img src="img/item-carousel/left.jpg" alt="Левая стрелка">
+            <span class="glyphicon glyphicon-chevron-left"></span>
         </a>
         <!--END-->
         <!--RIGHT-->
         <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <img src="img/item-carousel/right.jpg" alt="Правая стрелка">
+            <span class="glyphicon glyphicon-chevron-right"></span>
         </a>
         <!--END-->
     </div>
