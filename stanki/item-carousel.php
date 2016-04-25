3eacfe46ed0f43da381a6fb40fc4966c53fb6019
klimychevmn <plagonist@gmail.com>
2016-04-25 17:04:52 +0300

    fix carousel img

--- stanki/item-carousel.php
+++ stanki/item-carousel.php
@@ -3,31 +3,31 @@
         <!--MAIN IMGS-->
         <div class="carousel-inner" >
             <div class="item active" >
-                <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/img//kpp/molot/6.jpg">
+                <span class="img_carousel"><img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/img//kpp/molot/6.jpg"></span>
                 <div class="carousel-caption">
                     <span>Hello</span>
                 </div>
             </div>
             <div class="item" >
-                <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_2tzqc6vcobaiewt" >
+                <span class="img_carousel"><img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_2tzqc6vcobaiewt" ></span>
                 <div class="carousel-caption">
                     <span>My friend</span>
                 </div>
             </div>
             <div class="item" >
-                <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_7gdscjd36y73tau" >
+                <span class="img_carousel"><img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_7gdscjd36y73tau" ></span>
                 <div class="carousel-caption">
                     <span>Glad to see</span>
                 </div>
             </div>
             <div class="item" >
-                <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_xvl5iixg3qt7aw4" >
+                <span class="img_carousel"><img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_xvl5iixg3qt7aw4" ></span>
                 <div class="carousel-caption">
                     <span>You!</span>
                 </div>
             </div>
             <div class="item" >
-                <img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_leunhqd2e4h3gp0" >
+                <span class="img_carousel"><img title="МА4127" alt="МА4127" src="http://new.stanki-zavod.ru/fullimg/kpp_leunhqd2e4h3gp0" ></span>
                 <div class="carousel-caption">
                     <span>:)</span>
                 </div>
