b4bd5e4e71c07da19728676300eea4657a49d41b
klimychevmn <plagonist@gmail.com>
2016-04-28 15:15:14 +0300

    carousel helper

---
+++ alternativa-old/carousel.php
@@ -0,0 +1,60 @@
+<div class="container">
+    <div id="itemCarousel" class="carousel slide">
+
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="img/rabitsa/Рабица%20(ОЦ)20140708_104631.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/rabitsa/Рабица%20(ПНД)20150422_082040.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/rabitsa/Рабица%20ОЦ%20(1,2х10м)20140708_104828.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya/Сварная%20(100х100)20140708_111836.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya/Сварная%20(50х50)%2020140708_110023%5B1%5D.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya/Сварная%20(200х200)20140708_111144.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya/Сетка%20(50х50х4)20140708_105839.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya/Сетка%20(150х150)20140708_105238.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya/(100х100х5)20140708_105850.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya-rulon/Рулон.сварная20150312_092002.jpg"  alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya-rulon/Сетка%20(12,5х12,5)20150422_082315.jpg"  alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya-rulon/Сетка%20рулон.%20(50х50х1,6)20150408_081430.jpg"  alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya-rulon/Сетка%20рулон.%20(50х50х1,6)20150408_081451.jpg"  alt="">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya-rulon/Сетка%20сварная%20(25х25х1,6).jpg"  alt="new slide">
+            </div>
+            <div class="item">
+                <img src="img/svarnaya-rulon/Сетка%20сварная%20(50х12,5х1,8)20151013_122413.jpg"  alt="">
+            </div>
+        </div>
+
+        <a class="left carousel-control" href="#itemCarousel" data-slide="prev"><span
+                class="glyphicon glyphicon-chevron-left"></span></a>
+        <a class="right carousel-control" href="#itemCarousel" data-slide="next"><span
+                class="glyphicon glyphicon-chevron-right"></span></a>
+        <div class="clear"></div>
+
+    </div>
+
+</div>
