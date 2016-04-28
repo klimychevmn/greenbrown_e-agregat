45838653207140d968aa26be746afb7198b8a2a3
klimychevmn <plagonist@gmail.com>
2016-04-28 15:10:35 +0300

    fixed item button fixed item carousel img

--- alternativa-old/items.php
+++ alternativa-old/items.php
@@ -3,30 +3,13 @@
 
 <div class="container">
     <div class="item">
-        <h3 class="text-center">Выберете товар, о котором хотели бы узнать больше</h3>
-        <a href="item-rabitsa.php">
-            <div class="col-lg-6 col-md-6 text-center">
-                <div class="row">
-                    <img src="img/rabitsa/Рабица%20(ПНД)20150422_082040.jpg" alt="Сетка Рабица">
-                </div>
-                <div class="row">
-                    <h2>Сетка рабица</h2>
-                </div>
-            </div>
-        </a>
-        <a href="item-svarnaya.php">
-            <div class="col-lg-6 col-md-6 text-center">
-                <div class="row">
-                    <img src="img/svarnaya/Сварная%20(100х100)20140708_111836.jpg" alt="Сетка Сварная" >
-                </div>
-                <div class="row">
-                    <h2>Сетка сварная</h2>
-                </div>
-            </div>
-        </a>
         <div class="price row text-center">
-            <a href="#">Скачать прайс-лист</a>
+            <a href="#" class="btn btn-primary">Скачать прайс-лист</a>
         </div>
+        <h3 class="text-center">Галерея товаров. Сетка сварная. Сетка рабица</h3>
+        
+        <?php require('carousel.php');?>
+
     </div>
 </div>
 
