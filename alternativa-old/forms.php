5e63a38870b7f4c2c731794dad1d933ad4cc8616
klimychevmn <plagonist@gmail.com>
2016-04-29 16:36:15 +0300

    доделал отправку формы

--- alternativa-old/forms.php
+++ alternativa-old/forms.php
@@ -3,7 +3,7 @@
 
 <div class="my_container">
     <div class="forms">
-        <form class="form-horizontal">
+        <form id="form" class="form-horizontal">
             <div class="form-group">
                 <label class="col-sm-2 control-label">ФИО</label>
                 <div class="col-sm-10">
@@ -40,7 +40,7 @@
                     <!-- Заголовок модального окна -->
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                            <h3 class="modal-title">Принять участие в Фестивале</h4>
+                            <h3 class="modal-title">Уведомление об отправлении информации</h4>
                         </div>
                     <!-- Основное содержимое модального окна -->
                         <div class="modal-body">
--- alternativa-old/sendformsinfo.php
+++ alternativa-old/sendformsinfo.php
@@ -1,21 +1,27 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")&&(isset($_POST['comment'])&&$_POST['comment']!="")){ 
-    $to = 'plagonist@gmail.com'; 
-    $subject = 'Проверка связи Алтернатива-СВД'; 
-    $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>'.$subject.'</p>
-                        <p>ФИО: '.$_POST['name'].'</p>
-                        <p>e-mail: '.$_POST['email'].'</p>
-                        <p>Комментарий: '.$_POST['comment'].'</p>                        
-                    </body>
-                </html>'; 
-    $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
-    $headers .= "From:<".$_POST['email'].">\r\n"; 
-    mail($to, $subject, $message, $headers); 
-}
+if((isset($_POST['name'])&&$_POST['name']!="")&&
+    (isset($_POST['comment'])&&$_POST['comment']!="")&&
+    (isset($_POST['email'])&&$_POST['email']!="")&&
+    (isset($_POST['phone'])&&$_POST['phone']!="")){ 
+        $to = 'roydash@mail.ru'; 
+        $subject = 'Проверка связи Алтернатива-СВД тест'; 
+        $message = '
+                    <html>
+                        <head>
+                            <title>'.$subject.'</title>
+                        </head>
+                        <body>
+                            <p>Привет, крошка!</p>
+                            <p>Я тут решил протестировать форму обратной связи для сайта Алтернатива-СВД</p>
+                            <p>Как ты видишь, я очень крут. Давай сходим погулять? <br> Я оставлю тут свои координаты, которые я вбил через форму. ^.^</p>
+                            <p>ФИО: '.$_POST['name'].'</p>
+                            <p>Интересующий вопрос: '.$_POST['comment'].'</p>                        
+                            <p>e-mail: '.$_POST['email'].'</p>
+                            <p>Телефон: '.$_POST['phone'].'</p>
+                        </body>
+                    </html>'; 
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+        $headers .= "From:<".$_POST['email'].">\r\n"; 
+        mail($to, $subject, $message, $headers); 
+    }
 ?>
