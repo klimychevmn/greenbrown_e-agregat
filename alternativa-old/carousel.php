405153faa08e4363804091584024d1f96f9dd837
klimychevmn <plagonist@gmail.com>
2016-05-02 20:46:54 +0300

    перекинул все фотки в альбом в вк

--- alternativa-old/carousel.php
+++ alternativa-old/carousel.php
@@ -3,105 +3,105 @@
 
         <div class="carousel-inner">
             <div class="item active">
-                <img src="img/rabitsa/Рабица%20(ОЦ)20140708_104631.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/598c/N9FtsW_La3Q.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Рабица</h3>
                     <p>ОЦ</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/rabitsa/Рабица%20(ПНД)20150422_082040.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/5996/eQDcU6CZNAE.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Рабица</h3>
                     <p>ПНД</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/rabitsa/Рабица%20ОЦ%20(1,2х10м)20140708_104828.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/59a0/3l_pigmobiQ.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Рабица</h3>
                     <p>ОЦ (1,2х10м)</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya/Сварная%20(100х100)20140708_111836.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/5964/mxV9UyZ4IEs.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Сварная</h3>
                     <p>100х100</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya/Сварная%20(50х50)%2020140708_110023%5B1%5D.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/595a/hQDqTxWRQVU.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Сварная</h3>
                     <p>50х50</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya/Сварная%20(200х200)20140708_111144.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/596e/CQqh9W2Zs8Y.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Сварная</h3>
                     <p>200х200</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya/Сетка%20(50х50х4)20140708_105839.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/5978/x__WG-vK6ec.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Сетка</h3>
                     <p>50х50х4</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya/Сетка%20(150х150)20140708_105238.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/5982/1xwZ9vIBqhg.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Сетка</h3>
                     <p>150х150</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya/(100х100х5)20140708_105850.jpg" alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/5950/r3cJ5XGcC_Q.jpg" alt="">
                 <div class="carousel-caption">
                     <h3>Сварная</h3>
                     <p>100х100х5</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya-rulon/Рулон.сварная20150312_092002.jpg"  alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/590b/pWZNbYYafEw.jpg"  alt="">
                 <div class="carousel-caption">
                     <h3>Сварная</h3>
                     <p>Рулон</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya-rulon/Сетка%20(12,5х12,5)20150422_082315.jpg"  alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/5915/9QPQevZ7WLw.jpg"  alt="">
                 <div class="carousel-caption">
                     <h3>Сетка</h3>
                     <p>12,5х12,5</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya-rulon/Сетка%20рулон.%20(50х50х1,6)20150408_081430.jpg"  alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/591f/XAvpl8o6e1g.jpg"  alt="">
                 <div class="carousel-caption">
                     <h3>Сетка рулон</h3>
                     <p>50х50х1,6</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya-rulon/Сетка%20рулон.%20(50х50х1,6)20150408_081451.jpg"  alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/5929/YY_GzUdMyHA.jpg"  alt="">
                 <div class="carousel-caption">
                     <h3>Сетка рулон</h3>
                     <p>50х50х1,6</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya-rulon/Сетка%20сварная%20(25х25х1,6).jpg"  alt="new slide">
+                <img src="https://pp.vk.me/c636524/v636524442/5933/i3coJv9LPo0.jpg"  alt="new slide">
                 <div class="carousel-caption">
                     <h3>Сетка сварная</h3>
                     <p>25х25х1,6</p>
                 </div>
             </div>
             <div class="item">
-                <img src="img/svarnaya-rulon/Сетка%20сварная%20(50х12,5х1,8)20151013_122413.jpg"  alt="">
+                <img src="https://pp.vk.me/c636524/v636524442/593d/LynctJ8OX50.jpg"  alt="">
                 <div class="carousel-caption">
                     <h3>Сетка сварная</h3>
                     <p>50х12,5х1,8</p>
